450b82bd72d1f144993f9488282ce286e1abbddc
catch762 <hoh.prichesat@gmail.com>
2026-03-04 21:25:03 +0300

    delete fucking MV

--- src/main.cpp
+++ src/main.cpp
@@ -9,43 +9,23 @@
 #include "sv_datalayer.h"
 
 
-
-DataNodeShared getroot()
+void test3()
 {
     auto root       = std::make_shared<DataNode>(QString("root"), DataNode::NodeType::Composite);
     auto child      = std::make_shared<DataNode>(QString("child"), DataNode::NodeType::Composite);
     auto grandchild = std::make_shared<DataNode>(QString("grandchild"), DataNode::NodeType::Leaf);
     *grandchild->tryGetLeafvalue() = QString("okookko");
-    
     root->addChild(child);
     child->addChild(grandchild);
-
-    return root;
-}
-
-void test2()
-{
-    auto root = getroot();
-
-    auto fail = root->tryGetChild(7);
-
-    // Create model and view
-    DataTreeModel *model = new DataTreeModel(root);
-    QTreeView *treeView = new QTreeView();
-    //treeView->setEditTriggers(QAbstractItemView::DoubleClicked | QAbstractItemView::EditKeyPressed);  // ← CRITICAL
-    treeView->setModel(model);
-    treeView->expandAll();  // Optional: expand all nodes
-
-    treeView->show();
 }
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    SV_LOG("--- app launch --- " + getCurrentTimeHMS())
+    SV_LOG("\n\n--- app launch --- " + getCurrentTimeHMS() + "\n")
 
-    test2();
+    test3();
 
     auto res = app.exec();
     qDebug() << "app.exec() returned " << res;
