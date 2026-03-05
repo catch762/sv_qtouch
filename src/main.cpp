97e3909339ad05f5cc8cf381e607b9a1a456e32c
catch762 <hoh.prichesat@gmail.com>
2026-03-05 21:08:14 +0300

    nothing much

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,57 @@ void test3()
     *grandchild->tryGetLeafvalue() = QString("okookko");
     root->addChild(child);
     child->addChild(grandchild);
+
+
+    DataLayerSystem::instance().registerWidgetMaker<QString>(
+        [](DataNodeShared node) -> QWidget*
+        {
+            if (!node)
+            {
+                SV_ERROR("WidgetMakers", "Failed to make widget: null node passed in");
+                return nullptr;
+            }
+
+            auto leafValue = node->tryGetLeafvalue();
+            if (!leafValue)
+            {
+                SV_ERROR("WidgetMakers", "Failed to make widget: node isnt even leaf, its: " + node->stdBasicInfo());
+                return nullptr;
+            }
+
+            if (qMetaTypeId<QString>() != leafValue->typeId())
+            {
+                //todo better log
+                SV_ERROR("WidgetMakers", "Failed to make widget: its a leaf but types mismatch");
+                return nullptr;
+            }
+
+            auto *widget = new QLineEdit(leafValue->toString());
+
+            auto nodeWeak = DataNodeWeak(node);
+
+            //todo check if 3rd param...
+            QObject::connect(widget, &QLineEdit::textChanged, widget, [nodeWeak](const QString &s)
+            {
+                if (auto nodeShared = nodeWeak.lock())
+                {
+                    if (auto leaf = nodeShared->tryGetLeafvalue())
+                    {
+                        *leaf = s;
+                    }
+                    else
+                    {
+                        SV_ASSERT(false);
+                    }
+                }
+            });
+
+            return widget;
+        }
+    );
+
+    auto w = DataLayerSystem::instance().getWidgetForNode(grandchild);
+    w->show();
 }
 
 int main(int argc, char *argv[])
