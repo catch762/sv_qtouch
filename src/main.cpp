d13b7eeb82ae3ba97e2a5351dd04f4acf6640e3c
catch762 <hoh.prichesat@gmail.com>
2026-03-14 22:50:08 +0300

    +, also changed launch.json for debugging...

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,29 @@ void test_widgets()
     auto root   = DataNode::makeComposite("root");
     auto child  = root->addComposite("child");
     auto grand0 = child->addLeaf("grand0_name", QString("qstring text"));
-    auto grand1 = child->addLeaf("grand1_name", LimitedDouble{});
+    auto grand1 = child->addLeaf("grand1_name", LimitedDouble{6, 5, 7});
+    auto grand2 = child->addLeaf("grand2_name", LimitedDoubleVec{
+        LimitedDouble{6, 5, 7}, LimitedDouble{50, 0, 100}, LimitedDouble{}
+    });
 
-    auto w = WidgetMakerSystem::instance().makeWidgetForNode(grand1);
+    auto w = WidgetMakerSystem::instance().makeWidgetForNode(grand2);
     w->show();
+}
+
+void test_vec()
+{
+    LimitedDoubleVec vec{ LimitedDouble{}, LimitedDouble{2,2,2}};
+    auto widget = new LimitedDoubleVecWidget(vec);
+    QObject::connect(widget, &LimitedDoubleVecWidget::valueChanged, [](const auto &vec)
+    {
+        SV_LOG("Valchanged, size " + std::to_string(vec.size()));
+    });
+
+    //vec.push_back(LimitedDouble());
+    vec.pop_back();
+    widget->setValue(vec);
+
+    widget->show();
 }
 
 int main(int argc, char *argv[])
@@ -28,6 +47,7 @@ int main(int argc, char *argv[])
     //AdhocTesting::runTest();
 
     test_widgets();
+    //test_vec();
 
     auto res = app.exec();
     
