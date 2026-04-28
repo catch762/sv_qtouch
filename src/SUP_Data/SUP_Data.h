191e80daba99436349b0233d72c6cfbafc235bf6
catch762 <hoh.prichesat@gmail.com>
2026-04-28 18:38:58 +0300

    comments, doc

--- src/SUP_Data/SUP_Data.h
+++ src/SUP_Data/SUP_Data.h
@@ -1,6 +1,15 @@
 #pragma once
 #include "sv_qtcommon.h"
 
+//******************************************************************************************************
+// These structs represent all the data we obtain from parsing GLSL code.
+//
+// 'SUP', or Svvvv Uniform Protocol is my small lib for GLSL - a collection of macros, mostly.
+// It does not belong in this project, but for a reference, i copied it to /doc/sup.glsl 
+// 
+// When we parse data from GLSL, we are looking for specific macros defined in SUP.
+// Hence i named the structs SUP_Something, rather than GLSL_Something - it reflects the intent better.
+//******************************************************************************************************
 struct SUP_StructMember
 {
     QString varType;
@@ -34,17 +43,24 @@ struct SUP_VarListEntry
     MacroType   macroType;
     QString     varType;
     QString     varName;
-    QStringOpt  uiMacroArg;
+    QStringOpt  uiMacroArg; //not including quotes
 
     std::string toString() const;
 };
 SV_DECL_OPT(SUP_VarListEntry);
 SV_DECL_STD_FORMATTER(SUP_VarListEntry, obj.toString());
 
+//********************************************************************
+// The final struct, it contains all the data we get from GLSL code.
+//********************************************************************
 struct SUP_Data
 {
     std::vector<SUP_StructDefinition>   structDefinitions;
-    std::vector<SUP_VarListEntry>       varListEntries;
+
+    // Final GLSL project uniform data layout - declaring variables, which are either: 
+    //  - of native type such as 'float' or 'vec4'
+    //  - a struct from the list of 'structDefinitions'
+    std::vector<SUP_VarListEntry>       varListEntries;     
 
     std::string toString() const;
 };
