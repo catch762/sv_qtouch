6065d7ed16053c7bb91df3fc17d85592ee223958
catch762 <hoh.prichesat@gmail.com>
2026-08-18 21:04:01 +0300

    tests

--- src/TreeUpdating/Tests/TreeUpdateTests.cpp
+++ src/TreeUpdating/Tests/TreeUpdateTests.cpp
@@ -35,16 +35,30 @@ TEST_CASE("Updating tree: simple insert new nodes")
 	CHECK(treesAreStructurallyEqual(*oldTree.get(), *newTree.get()));
 }
 
-TEST_CASE("Updating tree: renames")
+TEST_CASE("Updating tree: renames, insert, delete")
 {
 	auto oldTree = baseTree();
 	auto newTree = baseTree();
 	{
 		auto b = newTree->tryGetChild("b");
+		auto c = b->tryGetChild("c");
 
+		//1) rename b
 		b->setName("b_renamed");
-		b->tryGetChild("c")->setName("c_renamed");
-		b->tryGetChild("d")->setName("d_renamed");
+
+		//2) rename c
+		c->setName("c_renamed");
+
+		//3) add new child to b
+		b->addChild(
+			dnleaf("XXX_1", int(999)),
+			1
+		);
+
+		//4) delete last child (e)
+		auto children = newTree->tryGetCompositeData()->getChildren();
+		children.pop_back();
+		newTree->tryGetCompositeData()->setChildren(children, newTree);
 	}
 
 	TreeUpdater::tryUpdateOldSubtreeFromNew(oldTree, newTree);
